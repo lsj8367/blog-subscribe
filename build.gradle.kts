c89bdfbf91918d774b789d28c10179997eead0e3
lsj8367 <lsj8367@naver.com>
2024-01-05 00:40:38 +0900

    feat: add rome library

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("com.rometools:rome:2.1.0")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
