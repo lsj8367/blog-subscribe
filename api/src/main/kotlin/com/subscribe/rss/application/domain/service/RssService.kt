32131a1dc18bf8c9f0ff66c322c401fe8770ea84
lsj8367 <lsj8367@naver.com>
2024-01-10 00:45:49 +0900

    feat: add rss usecase

--- api/src/main/kotlin/com/subscribe/rss/application/domain/service/RssService.kt
+++ api/src/main/kotlin/com/subscribe/rss/application/domain/service/RssService.kt
@@ -6,6 +6,8 @@ import com.subscribe.rss.application.port.`in`.request.RssCreateRequest
 import com.subscribe.rss.application.port.out.RssRepository
 import com.subscribe.rss.application.port.out.response.RssCreateResponse
 import org.springframework.stereotype.Service
+import java.net.URI
+import java.net.URL
 
 @Service
 class RssService(
@@ -14,6 +16,12 @@ class RssService(
     override fun save(rssCreate: RssCreateRequest): RssCreateResponse {
         val save = rssRepository.save(Rss(rssCreate.url, true))
         return RssCreateResponse(save.id, save.uri, save.activated)
+    }
+
+    override fun getFeeds(): List<URL> {
+        val rssList = rssRepository.findActivatedRssList()
+        return rssList.map { rss -> URI.create(rss.uri).toURL() }
+            .toList()
     }
 
 }
--- api/src/main/kotlin/com/subscribe/rss/application/port/in/RssUseCase.kt
+++ api/src/main/kotlin/com/subscribe/rss/application/port/in/RssUseCase.kt
@@ -2,7 +2,9 @@ package com.subscribe.rss.application.port.`in`
 
 import com.subscribe.rss.application.port.`in`.request.RssCreateRequest
 import com.subscribe.rss.application.port.out.response.RssCreateResponse
+import java.net.URL
 
 interface RssUseCase {
     fun save(rssCreate: RssCreateRequest): RssCreateResponse
+    fun getFeeds(): List<URL>
 }
