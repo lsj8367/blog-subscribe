5e70a760b2d1cd81933c387d1f80b689b7f20349
lsj8367 <lsj8367@naver.com>
2024-01-26 22:11:34 +0900

    feat: add libraries

--- rss-batch/build.gradle.kts
+++ rss-batch/build.gradle.kts
@@ -2,6 +2,8 @@ dependencies {
     implementation(project(":api"))
     implementation(project(":common"))
     implementation("com.rometools:rome:2.1.0")
+    implementation("org.jsoup:jsoup:1.17.2")
+
 }
 
 tasks.getByName("bootJar") {
