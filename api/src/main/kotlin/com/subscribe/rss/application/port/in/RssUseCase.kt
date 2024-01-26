545919408fd2f33555000eab0ac557769b2d61be
lsj8367 <lsj8367@naver.com>
2024-01-26 22:11:03 +0900

    refactor: return object

--- api/src/main/kotlin/com/subscribe/rss/application/port/in/RssUseCase.kt
+++ api/src/main/kotlin/com/subscribe/rss/application/port/in/RssUseCase.kt
@@ -2,9 +2,9 @@ package com.subscribe.rss.application.port.`in`
 
 import com.subscribe.rss.application.port.`in`.request.RssCreateRequest
 import com.subscribe.rss.application.port.out.response.RssCreateResponse
-import java.net.URL
+import com.subscribe.rss.application.port.out.response.RssLinkInfo
 
 interface RssUseCase {
     fun save(rssCreate: RssCreateRequest): RssCreateResponse
-    fun getFeeds(): List<URL>
+    fun getFeeds(): List<RssLinkInfo>
 }
